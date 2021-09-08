760cdd533942d176b224fd1b75343195f53cfe61
Swapnil <swapniljadhav261991@gmail.com>
2021-09-08 06:26:18 +0530

    Simple Factory

---
+++ DesignPatterns/src/com/cpc/dp/simplefactory/Client.java
@@ -0,0 +1,11 @@
+package com.cpc.dp.simplefactory;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Post post = PostFactory.createPost("news");
+		System.out.println(post);
+
+	}
+
+}
