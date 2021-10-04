ee52da629ae059f092a6a88c309dc5d5517596f8
Swapnil <swapniljadhav261991@gmail.com>
2021-10-05 01:54:52 +0530

    design pattern

--- DesignPatterns/src/com/cpc/dp/facade/Client.java
+++ DesignPatterns/src/com/cpc/dp/facade/Client.java
@@ -1,17 +1,21 @@
 package com.cpc.dp.facade;
 
-import com.cpc.dp.facade.email.EmailFacade;
+import java.util.stream.Stream;
 
 public class Client {
 
 	public static void main(String[] args) {
-		Order order = new Order("101", 99.99);
+		/*Order order = new Order("101", 99.99);
 		EmailFacade facade = new EmailFacade();
 		
 		boolean result = facade.sendOrderEmail(order);
 		
-		System.out.println("Order Email "+ (result?"sent!":"NOT sent..."));
-		
+		System.out.println("Order Email "+ (result?"sent!":"NOT sent..."));*/
+		String[] names = COLUMNS.names();
+		for (String name : names) {
+			System.out.println(name+" ");
+		}
+
 	}
 
 //	private static boolean sendOrderEmailWithoutFacade(Order order) {
@@ -34,4 +38,24 @@ public class Client {
 		}
 	};
 
+}
+
+enum COLUMNS {
+	UUID,
+	PARENT_ID,
+	ER,
+	VERSION;
+
+	public static String [] names() {
+		//return Stream.of(COLUMNS.values()).map(COLUMNS::name).toArray(String[]::new);
+		Stream<COLUMNS> values = Stream.of(COLUMNS.values());
+		Stream<String> stringStream = values.map(COLUMNS::name);
+		String[] strings = stringStream.toArray(String[]::new);
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
 }
