4ce2d5a97d19884070a3e2b134abe6ad7919c745
Swapnil <swapniljadhav261991@gmail.com>
2021-10-08 08:39:35 +0530

    design pattern

---
+++ DesignPatterns/src/com/cpc/dp/proxy/dynamic/Client.java
@@ -0,0 +1,17 @@
+package com.cpc.dp.proxy.dynamic;
+
+import com.cpc.dp.proxy.Image;
+
+import javafx.geometry.Point2D;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Image img = ImageFactory.getImage("A.bmp");
+		img.setLocation(new Point2D(-10, 0));
+		System.out.println(img.getLocation());
+		System.out.println("*****************************");
+		img.render();
+		
+	}
+}
